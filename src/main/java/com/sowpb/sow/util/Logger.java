6d00ca5698d00261443366c84989851b057b7df6
Noah's Rrk <maxbeokes@gmail.com>
2017-09-02 17:45:20 -0700

    Basis

---
+++ src/main/java/com/sowpb/sow/util/Logger.java
@@ -0,0 +1,145 @@
+package com.sowpb.sow.util;
+
+import static com.sowpb.sow.util.Utils.createFile;
+import static com.sowpb.sow.util.Utils.getSysTimeStr;
+import static com.sowpb.sow.util.Utils.writeLine;
+
+import java.io.File;
+
+public class Logger {
+
+	/**
+	 * The log file.
+	 */
+	private File log = new File(".\\SOW-Log.txt");
+
+	/**
+	 * The thread that this logger is recording.
+	 */
+	private String thread;
+
+	public Logger() {
+		this.thread = "unknown";
+	}
+
+	public Logger(String _thread) {
+		this.thread = _thread;
+	}
+
+	/**
+	 * Create the log file. This should only be cased once. <br>
+	 * If it's cased for multiple times, the old log file <br>
+	 * will be deleted.
+	 */
+	public void logFile() {
+		createFile(log.toString(), true);
+	}
+
+	/**
+	 * Write the logging message to the log file.
+	 */
+	private void writeToLog(String msg) {
+		writeLine(log.toString(), msg);
+	}
+
+	/**
+	 * <i>Level: Info</i><br>
+	 * This type of message tells the user what is happening inside the <br>
+	 * program, and can be ignored.
+	 */
+	public void info(String msg) {
+		StringBuilder b = new StringBuilder();
+		b.append("[");
+		b.append(getSysTimeStr());
+		b.append("]");
+		b.append("[INFO]");
+		b.append("[Thread_");
+		b.append(thread);
+		b.append("]");
+		b.append(msg);
+		System.out.println(b.toString());
+		writeToLog(b.toString());
+	}
+
+	/**
+	 * The debug information is used by developers to determine <br>
+	 * whether some codes are correct or not.
+	 */
+	public void debug(String msg) {
+		StringBuilder b = new StringBuilder();
+		b.append("[");
+		b.append(getSysTimeStr());
+		b.append("]");
+		b.append("[DEBUG]");
+		b.append("[Thread_");
+		b.append(thread);
+		b.append("]");
+		b.append(msg);
+		System.out.println(b.toString());
+		writeToLog(b.toString());
+	}
+
+	/**
+	 * <i>Level: Warning</i> <br>
+	 * This type of message is used to show something that could hinder the
+	 * running <br>
+	 * of the program. Although they couldn't cause some major problem, users
+	 * should also <br>
+	 * check their properties to match the right way.
+	 */
+	public void warn(String msg) {
+		StringBuilder b = new StringBuilder();
+		b.append("[");
+		b.append(getSysTimeStr());
+		b.append("]");
+		b.append("[WARNING]");
+		b.append("[Thread_");
+		b.append(thread);
+		b.append("]");
+		b.append(msg);
+		System.out.println(b.toString());
+		writeToLog(b.toString());
+	}
+
+	/**
+	 * <i>Level: Error</i><br>
+	 * This type of message shows that something blocks the running of the
+	 * program. <br>
+	 * Users should look up the manual to fix it, or contact developers.
+	 */
+	public void error(String msg) {
+		StringBuilder b = new StringBuilder();
+		b.append("[");
+		b.append(getSysTimeStr());
+		b.append("]");
+		b.append("[ERROR]");
+		b.append("[Thread_");
+		b.append(thread);
+		b.append("]");
+		b.append(msg);
+		System.out.println(b.toString());
+		writeToLog(b.toString());
+	}
+
+	/**
+	 * <b><i>Level: Fatal</b></i> <br>
+	 * This type of message indicates the critical errors that can dump the <br>
+	 * whole program. Users should immediately report this type of message to
+	 * <br>
+	 * the developers.
+	 */
+	public void fatal(String msg) {
+		StringBuilder b = new StringBuilder();
+		b.append("[");
+		b.append(getSysTimeStr());
+		b.append("]");
+		b.append("[FATAL ERROR]");
+		b.append("[Thread_");
+		b.append(thread);
+		b.append("]");
+		b.append(msg);
+		System.out.println(b.toString());
+		writeToLog(b.toString());
+	}
+
+}
---
+++ src/main/java/com/sowpb/sow/util/Utils.java
@@ -0,0 +1,136 @@
+package com.sowpb.sow.util;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class Utils {
+
+	public static void createFile(String path, boolean override) {
+		File f = new File(path);
+		File parent = f.getParentFile();
+		if (parent != null && !parent.exists())
+			parent.mkdirs();
+		if (f.exists())
+			if (override == true)
+				f.delete();
+			else
+				return;
+		try {
+			f.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Read the context of a specified line in a text file.
+	 */
+	public static String readLineS(String filePath, int line) throws IOException {
+		File f = new File(filePath);
+		List<String> lines = Files.readAllLines(f.toPath());
+		return lines.get(line - 1);
+	}
+
+	/**
+	 * Read the context after a specified position of a line in a text file.
+	 */
+	public static String readLineSP(String filePath, int line, int pos) throws IOException {
+		pos--;
+		String l = readLineS(filePath, line);
+		if (l.length() < pos)
+			pos = l.length();
+		l = l.substring(pos, l.length());
+		return l;
+	}
+
+	/**
+	 * Get the content of a text file as a single string.
+	 */
+	public static String readFileToSingleString(String path) throws IOException {
+		List<String> lns = new ArrayList<>();
+		try {
+			lns = Files.readAllLines(new File(path).toPath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		StringBuilder sb = new StringBuilder();
+		for (String x : lns)
+			sb.append(x).append("\n");
+		return sb.toString();
+	}
+
+	/** Read the total lines of a text file. */
+	public static int getTotalLines(String filePath) throws IOException {
+		File f = new File(filePath);
+		List<String> lines = Files.readAllLines(f.toPath());
+		return lines.size() + 1;
+	}
+
+	/**
+	 * Write a line to a text file. Automatically start a new line after the
+	 * writing, <br>
+	 * but will NOT start writing on a new line!
+	 */
+	public static void writeLine(String filePath, String msg) {
+		File f = new File(filePath);
+		BufferedWriter out = null;
+		try {
+			out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(f, true)));
+			out.write(msg);
+			out.write("\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Write data to a specific line of a text file. <br>
+	 * Will override the old data.
+	 */
+	public static void writeLineS(String filePath, String msg, int line) throws IOException {
+		File f = new File(filePath);
+		List<String> lines = Files.readAllLines(f.toPath());
+		lines.set(line - 1, msg);
+		Files.write(f.toPath(), lines);
+	}
+
+	/**
+	 * Write data to a specific position of a specific line of a text file. <br>
+	 * The old data after the position will be erased. <br>
+	 */
+	public static void writeLineSP(String filePath, String msg, int line, int pos) throws IOException {
+		pos--;
+		line--;
+		File f = new File(filePath);
+		List<String> lines = Files.readAllLines(f.toPath());
+		String l = lines.get(line);
+		if (l.length() < pos)
+			pos = l.length();
+		l = l.substring(l.length() - pos);
+		l = l + msg;
+		lines.set(line, l);
+		Files.write(f.toPath(), lines);
+	}
+
+	/**
+	 * The system time in the format of hh:mm:ss.
+	 */
+	public static String getSysTimeStr() {
+		Calendar c = Calendar.getInstance();
+		return c.get(Calendar.HOUR_OF_DAY) + ":" + c.get(Calendar.MINUTE) + ":" + c.get(Calendar.SECOND);
+	}
+
+}
