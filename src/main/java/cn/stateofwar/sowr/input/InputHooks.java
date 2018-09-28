4f98f34b364457e0e1bc6383a80f2923cab1eba3
Noah's Rrk <maxbeokes@gmail.com>
2018-09-27 21:44:06 -0700

    Input handler

---
+++ src/main/java/cn/stateofwar/sowr/input/InputHooks.java
@@ -0,0 +1,49 @@
+package cn.stateofwar.sowr.input;
+
+/**
+ * Hooks for different type of inputs.
+ */
+public class InputHooks {
+
+	/**
+	 * A key has been triggered by listener. This method receives the parameters
+	 * from the listener automatically.
+	 * 
+	 * @param key      The key triggered.
+	 * 
+	 * @param scancode The system-specific scancode of the key.
+	 * 
+	 * @param action   The action of the key triggered.
+	 * 
+	 * @param mods     Which modifiers keys were held down.
+	 */
+	public static void keyboardTrigger(int key, int scancode, int action, int mods) {
+
+	}
+
+	/**
+	 * A mouse button has been triggered. This method receives the parameters from
+	 * the listener automatically.
+	 * 
+	 * @param button The button triggered.
+	 * 
+	 * @param action The action performed by the button.
+	 * 
+	 * @param mods   Which modifiers keys were held down.
+	 */
+	public static void mouseTrigger(int button, int action, int mods) {
+
+	}
+
+	/**
+	 * A cursor position listener.
+	 * 
+	 * @param x X position (in pixels) of the cursor.
+	 * 
+	 * @param y Y position (in pixels) of the cursor.
+	 */
+	public static void cursorTrigger(double x, double y) {
+
+	}
+
+}
---
+++ src/main/java/cn/stateofwar/sowr/input/KeyboardInput.java
@@ -0,0 +1,9 @@
+package cn.stateofwar.sowr.input;
+
+public class KeyboardInput {
+
+	static enum ModifierFlag {
+		NONE, CTRL, SHIFT, TAB, ALT;
+	}
+
+}
