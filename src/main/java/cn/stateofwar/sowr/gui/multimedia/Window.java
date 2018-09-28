72eef851aa0ca3d7dbeebe609c1cc4160d77bff5
Taxerap <maxbeokes@gmail.com>
2018-09-28 01:34:41 -0700

    [Questionable] Updated GLFW

--- src/main/java/cn/stateofwar/sowr/gui/multimedia/Window.java
+++ src/main/java/cn/stateofwar/sowr/gui/multimedia/Window.java
@@ -54,7 +54,7 @@ public class Window {
 		glfwWindowHint(GLFW_VISIBLE, GL_FALSE);
 		glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
