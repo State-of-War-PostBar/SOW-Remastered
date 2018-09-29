a6b4020d9c47892c137b32852dc29dc3b4932f41
Taxerap <maxbeokes@gmail.com>
2018-09-29 00:15:59 -0700

    World's stupidest orthographic coordinate conversion system

--- src/main/java/cn/stateofwar/sowr/util/DataUtils.java
+++ src/main/java/cn/stateofwar/sowr/util/DataUtils.java
@@ -6,7 +6,11 @@ import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 
 import org.joml.Matrix4f;
+import org.joml.Vector2f;
+import org.joml.Vector3f;
 import org.lwjgl.BufferUtils;
+
+import cn.stateofwar.sowr.gui.Graphic;
 
 /**
  * Utilities for data calculation and conversion. Mostly for OpenGL.
@@ -77,8 +81,6 @@ public class DataUtils {
 	 * @param far    Farthest back of the scene box.
 	 * 
 	 * @return The matrix of the orthographic data of the canonical view box.
-	 * 
-	 * @see glOrtho
 	 */
 	public static Matrix4f orthographic(float left, float right, float bottom, float top, float near, float far) {
 		Matrix4f ortho = new Matrix4f();
@@ -95,6 +97,93 @@ public class DataUtils {
 		ortho._m23(tz);
 
 		return ortho;
+	}
+
+	/**
+	 * Convert a pair of x-y coordinate of the screen (starts at the borrom left
+	 * corner, and x→, y↑) to OpenGL coordinates.
+	 * 
+	 * @param x The x coordinate.
+	 * 
+	 * @param y The y coordinate.
+	 * 
+	 * @return The OpenGL coordinate based on this pair of x-y coordinate.
+	 */
+	public static Vector3f toGlCoord(int x, int y) {
+		x = Math.abs(x);
+		y = Math.abs(y);
+
+		Vector2f vec = toScrPerc(x, y);
+
+		return new Vector3f(vec.x * 2.0f - 1.0f, vec.y * 2.0f - 1.0f, 0.0f);
+	}
+
+	/**
+	 * Convert a x-y coordinate to their ratios to the resolution of screen.
+	 * 
+	 * @param x The x coordinate.
+	 * 
+	 * @param y The y coordinate.
+	 * 
+	 * @return A vector contains their percentages toward the screen.
+	 */
+	public static Vector2f toScrPerc(int x, int y) {
+		x = Math.abs(x);
+		y = Math.abs(y);
+
+		float xPerc = (float) x / (float) Graphic.win.getWidth();
+		float yPerc = (float) y / (float) Graphic.win.getHeight();
+
+		return new Vector2f(xPerc, yPerc);
+	}
+
+	/**
+	 * Determine the quadrant of a x-y coordinate.
+	 * 
+	 * @param x The x coordinate.
+	 * 
+	 * @param y The y coordinate.
+	 * 
+	 * @return A number indicates the quadrant of the coordinate.<br />
+	 *         0 -> At origin;<br />
+	 *         1 -> 1st quadrant;<br />
+	 *         2 -> 2nd quadrant;<br />
+	 *         3 -> 3rd quadrant;<br />
+	 *         4 -> 4th quadrant;<br />
+	 *         5 -> Middle of 1st and 2nd quadrant;<br />
+	 *         6 -> Middle of 2nd and 3rd quadrant;<br />
+	 *         7 -> Middle of 3rd and 4th quadrant;<br />
+	 *         8 -> Middle of 1st and 4th quadrant.<br />
+	 */
+	public static int getQuadrant(int x, int y) {
+		x = Math.abs(x);
+		y = Math.abs(y);
+
+		int xMid = Graphic.win.getWidth() / 2;
+		int yMid = Graphic.win.getHeight() / 2;
+
+		y = Graphic.win.getHeight() - y;
+
+		if (x == y && x == 0)
+			return 3;
+		else if (x == xMid && y == yMid)
+			return 0;
+		else if (x > xMid && y < yMid)
+			return 1;
+		else if (x < xMid && y < yMid)
+			return 2;
+		else if (x < xMid && y > yMid)
+			return 3;
+		else if (x > xMid && y > yMid)
+			return 4;
+		else if (x == xMid && y < yMid)
+			return 5;
+		else if (x < xMid && y == yMid)
+			return 6;
+		else if (x == xMid && y > yMid)
+			return 7;
+		else
+			return 8;
 	}
 
 }
