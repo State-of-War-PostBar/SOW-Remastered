45756c26802ee4b3c3c6d7b337c7828b89869e1f
Taxerap <maxbeokes@gmail.com>
2018-10-03 00:08:14 -0700

    Update code

--- src/main/java/cn/stateofwar/sowr/gui/multimedia/Window.java
+++ src/main/java/cn/stateofwar/sowr/gui/multimedia/Window.java
@@ -70,6 +70,7 @@ public class Window {
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+		glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
 
 		handle = glfwCreateWindow(width, height, title, fullScreen ? glfwGetPrimaryMonitor() : NULL, NULL);
 
