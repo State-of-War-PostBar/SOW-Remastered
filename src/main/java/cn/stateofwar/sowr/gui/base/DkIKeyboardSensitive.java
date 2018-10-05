0b32523a4cb35fb80c103cb50d4605f6c13be03b
Taxerap <maxbeokes@gmail.com>
2018-10-04 22:48:19 -0700

    Added keyboard input

---
+++ src/main/java/cn/stateofwar/sowr/gui/base/DkIKeyboardSensitive.java
@@ -0,0 +1,40 @@
+package cn.stateofwar.sowr.gui.base;
+
+/**
+ * An element that responses to keyboard inputs.
+ */
+public interface DkIKeyboardSensitive {
+
+	/**
+	 * Called when the registered key(s) is pressed.
+	 */
+	public void pressed();
+
+	/**
+	 * Called when the registered key(s) is not pressed.
+	 */
+	public void released();
+
+	/**
+	 * Register key(s) to the element.
+	 * 
+	 * @param keys Keys to register.
+	 */
+	public void registerKeys(int[] keys);
+
+	/**
+	 * Get the keys bind to the element.
+	 */
+	public int[] getKeys();
+
+	/**
+	 * Update keyboard status.
+	 */
+	public default void updateKey() {
+		for (int key : getKeys())
+			if (DkSInputs.DK_Keys[key])
+				pressed();
+		released();
+	}
+
+}
