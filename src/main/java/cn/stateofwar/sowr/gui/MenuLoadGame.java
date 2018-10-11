f5e0721580d8d287639d77fd27034ac77ac215fb
Taxerap <maxbeokes@gmail.com>
2018-10-11 01:10:56 -0700

    Added menus
    [Add] Added many base menus.

---
+++ src/main/java/cn/stateofwar/sowr/gui/MenuLoadGame.java
@@ -0,0 +1,15 @@
+package cn.stateofwar.sowr.gui;
+
+import org.joml.Vector4d;
+
+import cn.stateofwar.sowr.gui.render.Texture;
+
+/**
+ * Load saved games menu.
+ */
+public class MenuLoadGame extends Menu {
+
+	public MenuLoadGame(String _identifier, Vector4d coord, Texture texture) {
+		super(_identifier, coord, texture);
+	}
+}
---
+++ src/main/java/cn/stateofwar/sowr/gui/MenuSettings.java
@@ -0,0 +1,16 @@
+package cn.stateofwar.sowr.gui;
+
+import org.joml.Vector4d;
+
+import cn.stateofwar.sowr.gui.render.Texture;
+
+/**
+ * Settings menu.
+ */
+public class MenuSettings extends Menu {
+
+	public MenuSettings(String _identifier, Vector4d coord, Texture texture) {
+		super(_identifier, coord, texture);
+	}
+
+}
