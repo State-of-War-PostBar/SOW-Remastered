e3b0bfb3df8dfe91fde16c6ba19bd6b3a6f2f23f
Taxerap <maxbeokes@gmail.com>
2018-11-22 17:27:05 -0800

    Update references and code

--- README.md
+++ README.md
@@ -18,18 +18,19 @@ This is a project rebuilding an old game: *State of War* (2001), made by [Cypron
 
 For additional information reguarding developing, please look forward to these references:
 
-[(ZH_CN) 开发规范](docs/REGULATIONS_ZH.md)
+[（简体中文） 开发规范](docs/REGULATIONS_ZH.md)
 
-[(ZH_CN) 开发向导](docs/GUIDE_ZH.md)
+[（简体中文） 开发向导](docs/GUIDE_ZH.md)
 
-[(ZH_CN) 开发指南](docs/SPECIFICATIONS_ZH.md)
+[（简体中文） 开发指南](docs/SPECIFICATIONS_ZH.md)
 
-[(ZH_CN) 新特性](docs/COMPARISION_TO_OLD_SOW_ZH.md)
+[（简体中文） 新特性](docs/COMPARISION_TO_OLD_SOW_ZH.md)
 
-[(ZH_CN) 游戏默认键位](http://htmlpreview.github.io/?https://github.com/State-of-War-PostBar/SOW-Remastered/blob/master/docs/DEFAULT_KEY_BINDINGS_ZH.html)
+[（简体中文） 游戏默认键位](http://htmlpreview.github.io/?https://github.com/State-of-War-PostBar/SOW-Remastered/blob/master/docs/DEFAULT_KEY_BINDINGS_ZH.html)
 
 ---
 
 This project is published under the [LGPLv3 License](https://www.gnu.org/licenses/lgpl.html).
 
-For more information, contact us at the [State of War Baidu Postbar](https://tieba.baidu.com/f?kw=%E8%93%9D%E8%89%B2%E8%AD%A6%E6%88%92&fr=index) or join our QQ chat group 70410976. For developers, please join our development group 683409875.
+For more information, contact us at the [State of War Baidu Postbar](https://tieba.baidu.com/f?kw=%E8%93%9D%E8%89%B2%E8%AD%A6%E6%88%92&fr=index) or join our QQ chat group 70410976.  
+For developers, please join our development group 683409875 and join [our Gitter chatting](https://gitter.im/State-of-War-PostBar).
--- src/common.hpp
+++ src/common.hpp
@@ -1,15 +1,15 @@
 /**************************************************************************************
  *
  * State Of War Remastered:
- * A free software project attemping to reforge an old game.
- * Copyleft 2018 State Of War Baidu Postbar, all rights not reserved.
+ * A free software project to reforge an old game.
+ * Copyleft 2018 (C) State Of War Baidu Postbar, all rights not reserved.
  *
- * State Of War Remastered is a free software. You can redistribute it and modify it
+ * State Of War Remastered is a free software. You can freely redistribute and modify it
  * under GNU lesser General Public License as published by the Free Software Foundation.
  * In the time this line is written, the version of the document is 3, but you may
  * obtain and use any later version of the document released by the GNU group.
  *
- * The program is distributed without any warranty, use all features and code at your
+ * This program is distributed without any warranty, use all features and code at your
  * own risk. Since the project manager (aka Taxerap) sucks at coding and has the fact
  * of being a selfish and arrogant idiot, this program is most likely to be broken.
  * See GNU lesser Genral Public License for more detail on warranties.
@@ -22,11 +22,11 @@
  **************************************************************************************/
 
 #ifndef SOWR_COMMON_HPP
-#define SOWR_COMMON_HPP
+#define SOWR_COMMON_HPP 221118L
 
-#ifdef _WIN32 || defined (__WIN32__) || defined (WIN32)
+#ifdef _WIN32 || (defined (__CYGWIN__) && !defined (_WIN32))
     #define SOWR_WINDOWS
-#elif defined (__MACH__)
+#elif defined (__MACH__) && defined (__APPLE__)
     #define SOWR_OSX
     #define SOWR_POSIX
 #else
@@ -35,7 +35,11 @@
 #endif
 
 #ifdef _DEBUG
-    #define SOWR_DEBUG_MODE
+    #define SOWR_DEBUG
+#endif
+
+#if (-1)>>1 == -1
+    #define SOWR_RSHIFT_ARITH
 #endif
 
 #ifdef SOWR_WINDOWS
@@ -50,4 +54,4 @@ constexpr const char *PROGRAM_NAME = "State Of War Remastered";
 constexpr const char *VERSION_NAME = "Indev";
 constexpr const unsigned int BUILD_NUMBER = 1U;
 
-#endif  // SOWR_COMMON_HPP
+#endif    // SOWR_COMMON_HPP
